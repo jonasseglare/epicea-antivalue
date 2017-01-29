2724dc3f904a00ea497e07498d04f2ab0c1f80c7
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-01-29 11:33:41 +0100

    bra

--- src/epicea/antivalue/core.clj
+++ src/epicea/antivalue/core.clj
@@ -2,14 +2,23 @@
   (:import [epicea.antivalue AntivalueException])
   (:require [epicea.tag.core :as tag]))
 
+(declare compile-sub)
 
-(defn compile-seq [deps form] nil)
+(defn compile-seq [deps form] 
+  (map (compile-sub deps) form))
 
-(defn compile-vector [deps form] nil)
+(defn compile-vector [deps form]
+  (into [] (map (compile-sub deps) form)))
 
-(defn compile-map [deps form] nil)
+(defn compile-pair [deps]
+  (fn [pair]
+    (map (compile-sub deps) pair)))
 
-(defn compile-set [deps form] nil)
+(defn compile-map [deps form]
+  (into {} (map (compile-pair deps) form)))
+
+(defn compile-set [deps form]
+  (into #{} (map (compile-sub deps) form)))
 
 (defn compile-dep-sym [deps form] nil)
   
@@ -18,13 +27,15 @@
     (compile-dep-sym form)
     form))
 
-(defn compile-sub [deps form]
-  ((cond
-    (seq? form) compile-seq
-    (vector? form) compile-vector
-    (map? form) compile-map
-    (set? form) compile-set
-    :default compile-primitive) deps form))
+(defn compile-sub 
+  ([deps form]
+   ((cond
+      (seq? form) compile-seq
+      (vector? form) compile-vector
+      (map? form) compile-map
+      (set? form) compile-set
+      :default compile-primitive) deps form))
+  ([deps] #(compile-sub deps %)))
 
 (defmacro either [& form]
   (compile-sub #{} `(either ~@form)))
--- test/epicea/antivalue/core_test.clj
+++ test/epicea/antivalue/core_test.clj
@@ -2,6 +2,12 @@
   (:require [clojure.test :refer :all]
             [epicea.antivalue.core :refer :all]))
 
+(defmacro compiles-identically [x]
+  `(is (= ~x (compile-sub #{} ~x))))
+
 (deftest a-test
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (compiles-identically 1)
+    (compiles-identically [1 2 3])
+    (compiles-identically :a)
+    (compiles-identically 'b)))

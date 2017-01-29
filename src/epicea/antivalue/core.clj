df7055ddd07948779f78dbec4207b1f2edcf7af8
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-01-29 12:21:23 +0100

    good

--- src/epicea/antivalue/core.clj
+++ src/epicea/antivalue/core.clj
@@ -2,10 +2,61 @@
   (:import [epicea.antivalue AntivalueException])
   (:require [epicea.tag.core :as tag]))
 
+(defn either-sym? [f]
+  (or (= 'either f)
+      (= `either f)))
+
+(declare make)
+
+(defn make-sym? [f]
+  (or (= 'make f) 
+      (= `make f)))
+
+(def special-forms {'if :if ;; OK
+                    'do :do ;; OK
+                    'let* :let ;; OK
+                    'loop* :loop ;; OK
+                    'recur :recur ;; OK
+                    'throw :throw ;; OK
+                    'def :def ;; OK
+                    'var :var ;; OK
+                    'monitor-enter :monitor-enter ;; OK
+                    'monitor-exit :monitor-exit ;; OK
+                    'fn* :fn ;; OK
+                    'try :try ;; OK
+                    'catch :catch ;; OK
+                    'quote :quote ;; OK
+                    })
+
+(defn error [& s]
+  (throw (RuntimeException. (apply str s))))
+
+
 (declare compile-sub)
 
-(defn compile-seq [deps form] 
+(defn compile-basic-seq [deps form]
   (map (compile-sub deps) form))
+
+(defn compile-either [deps args]
+  (println "Compile either")
+  (if (empty? args)
+    nil
+    `(try ~(compile-sub deps (first args))
+          (catch AntivalueException ~(gensym)
+            ~(compile-either deps (rest args))))))
+
+(defn compile-make [deps [p on-true on-false]]
+  `(if ~(compile-sub deps p) 
+     ~(compile-sub deps on-true)
+     (throw (AntivalueException. ~(compile-sub deps on-false)))))
+
+(defn compile-seq [deps form] 
+  (let [f (first form)
+        args (rest form)]
+    (cond
+      (either-sym? f) (compile-either deps args)
+      (make-sym? f) (compile-make deps args)
+      :default (compile-basic-seq deps form))))
 
 (defn compile-vector [deps form]
   (into [] (map (compile-sub deps) form)))
@@ -20,11 +71,15 @@
 (defn compile-set [deps form]
   (into #{} (map (compile-sub deps) form)))
 
-(defn compile-dep-sym [deps form] nil)
+(defn unwrap-dep-value [x]  
+  (let [y (tag/value x)]
+    (if (tag/success? x)
+      y
+      (throw (AntivalueException. y)))))
   
 (defn compile-primitive [deps form]
   (if (contains? deps form)
-    (compile-dep-sym form)
+    `(unwrap-dep-value ~form)
     form))
 
 (defn compile-sub 
--- test/epicea/antivalue/core_test.clj
+++ test/epicea/antivalue/core_test.clj
@@ -1,5 +1,7 @@
 (ns epicea.antivalue.core-test
+  (:import [epicea.antivalue AntivalueException])
   (:require [clojure.test :refer :all]
+            [epicea.tag.core :as tag]
             [epicea.antivalue.core :refer :all]))
 
 (defmacro compiles-identically [x]
@@ -13,4 +15,14 @@
     (compiles-identically 'b)
     (compiles-identically #{1 :a 3})
     (compiles-identically {:a 3 :b 4})
-    (compiles-identically '(1 :a 3))))
+    (compiles-identically '(1 :a 3))
+    (is (= 9 (unwrap-dep-value (tag/tag-success 9))))
+    (try
+      (unwrap-dep-value (tag/tag-failure 9))
+      (is false)
+      (catch AntivalueException e
+        (is (= 9 (.state e)))))
+    (is (compile-sub #{'a} 'a))
+    (is (= 3 (either 3 9)))
+    (is (= 4 (either (make false 9 3) 4)))
+    (is (= 9 (either (make true 9 3) 4)))))

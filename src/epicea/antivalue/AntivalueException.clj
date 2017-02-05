03a90e28b6916d8061d5fc6304d97ad7d40388c9
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-02-05 19:46:27 +0100

    remove exception type

--- src/epicea/antivalue/AntivalueException.clj
+++
@@ -1,9 +0,0 @@
-(ns epicea.antivalue.AntivalueException
-  (:gen-class 
-   :extends java.lang.Exception
-   :state state
-   :init init
-   :constructors {[Object] []}))
-
-(defn -init [x]
-  [[] x])

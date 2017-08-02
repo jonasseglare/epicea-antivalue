2b3d21579e4363c458420395d740890577c243b3
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-08-02 08:26:52 +0200

    asdf

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
-(defproject epicea/antivalue "0.1.0-SNAPSHOT"
+(defproject bluebell/antivalue "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
-                 [epicea/utils "0.1.0-SNAPSHOT"]
-                 ])
+                 [bluebell/utils "0.1.0-SNAPSHOT"]])

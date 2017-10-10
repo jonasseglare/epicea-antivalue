39d1d24352fbffc688f454090750734f753041f5
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-10-10 07:30:58 +0200

    RELEASE

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject bluebell/antivalue "0.1.0-SNAPSHOT"
+(defproject bluebell/antivalue "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
-                 [bluebell/utils "0.1.0-SNAPSHOT"]])
+                 [bluebell/utils "0.1.0"]])
